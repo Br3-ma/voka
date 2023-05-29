c6a7bef4e96d94baa00b7d920d9b05ed839e3337
br3-mah <nyeleti.bremah@gmail.com>
2023-05-29 14:51:12 +0200

    Official Logo Icon

--- app/resources/views/layouts/head.blade.php
+++ app/resources/views/layouts/head.blade.php
@@ -11,6 +11,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
         <!-- Styles -->
+        <link rel="icon" type="image/png" href="{{ asset('public/img/logo.png') }}">
         <link rel="stylesheet" src="{{ asset('public/assets/css/footer.css') }}">
         <script src="https://cdn.tailwindcss.com"></script>
         
@@ -21,10 +22,8 @@
             <div class="border py-3 px-6">
                 <div class="flex justify-between">
                     <a href="{{ route('welcome') }}" class="flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
-                        </svg>
-                        <span class="ml-2 font-semibold text-[#252C32]">Vokamba</span>
+                        <img class="h-10 w-10 text-red-500" src="{{ asset('public/img/logo.png') }}">
+                        {{-- <span class="ml-2 font-semibold text-[#252C32]">Vokamba</span> --}}
                     </a>
             
                     <div class="ml-6 flex flex-1 gap-x-3">
