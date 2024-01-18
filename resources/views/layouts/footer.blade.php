00497b6149d671a78d3201a447331fad1ab8f0db
br3-mah <nyeleti.bremah@gmail.com>
2024-01-18 12:30:33 +0200

    Clean Mobile Responsive website

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -6,7 +6,7 @@
     @livewireScripts
     </body>
 
-    <div class="max-w-2xl mx-auto">
+    <div class="w-full">
     
       <footer class="p-4 bg-white rounded-lg md:flex md:items-center md:justify-between md:p-6">
         <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://vookamba.com" class="hover:underline" target="_blank">Vookamba™</a>. All Rights Reserved.
